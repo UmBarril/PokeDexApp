533cc6b69291caf2298a1f503327fec45752de33
joao.barros <joao.barros@phoebustecnologia.com.br>
2024-09-03 15:06:55 -0300

    inicio PokemonResponse e PokemonRepository

---
+++ app/src/main/java/com/barril/pokedexapp/data/PokemonRepository.kt
@@ -0,0 +1,21 @@
+package com.barril.pokedexapp.data
+
+import com.barril.pokedexapp.data.model.PokemonResponse
+
+class PokemonRepository {
+    fun getPokemons(amount: Int): List<PokemonResponse> {
+        TODO()
+    }
+
+    fun searchPokemon(name: String): List<PokemonResponse> {
+        TODO()
+    }
+
+    fun getFavoritePokemons(amount: Int): List<PokemonResponse> {
+        TODO()
+    }
+
+    fun searchFavoritePokemon(name: String): List<PokemonResponse> {
+        TODO()
+    }
+}
