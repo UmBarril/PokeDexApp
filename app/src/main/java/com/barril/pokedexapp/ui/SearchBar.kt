625b054c3e43b9f09207cc7a9de1b98f09ba1bb6
joao.barros <joao.barros@phoebustecnologia.com.br>
2024-09-06 14:44:18 -0300

    adicionando strings ao strings.xml

--- app/src/main/java/com/barril/pokedexapp/ui/SearchBar.kt
+++ app/src/main/java/com/barril/pokedexapp/ui/SearchBar.kt
@@ -10,19 +10,25 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.barril.pokedexapp.R
 
 @Composable
-fun SearchBar(modifier: Modifier = Modifier) {
+fun SearchBar(
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
     TextField(
         value = "",
-        onValueChange = { /* TODO */ },
+        onValueChange = onValueChange,
         leadingIcon = {
             Icon(imageVector = Icons.Default.Search, contentDescription = null)
         },
         colors = TextFieldDefaults.colors(),
         placeholder = {
-            Text("Pesquisar")
+            Text(stringResource(R.string.search))
         },
         modifier = modifier
             .fillMaxWidth()
