abe58810bfaa73d1bd73b05a8a0d9aa7b04dda57
joao.barros <joao.barros@phoebustecnologia.com.br>
2024-09-06 15:32:42 -0300

    corrigindo aparencia da PokemonSearchBar e colocando mais strings no strings.xml

--- app/src/main/java/com/barril/pokedexapp/ui/HomeView.kt
+++ app/src/main/java/com/barril/pokedexapp/ui/HomeView.kt
@@ -1,16 +1,34 @@
 package com.barril.pokedexapp.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.barril.pokedexapp.PokemonType
 import com.barril.pokedexapp.R
 import com.barril.pokedexapp.ui.theme.PokeDexAppTheme
@@ -21,16 +39,59 @@ import java.util.EnumSet
 fun HomeView(modifier: Modifier = Modifier) {
     Column {
         PokemonSearchBar(modifier)
-        PokemonColumnList(modifier)
+        Surface {
+            PokemonColumnList(modifier.weight(1f))
+        }
     }
 }
 
 @Composable
 fun PokemonSearchBar(modifier: Modifier = Modifier) {
-    Row() {
-        SearchBar()
-        Button(onClick = { /*TODO*/ }) {
-            Icon(painterResource(id = R.drawable.filter_icon), contentDescription = "Filtros")
+    var isSearchingNow by remember { mutableStateOf(false) }
+    Surface {
+        Row(
+            horizontalArrangement = Arrangement.End,
+            modifier = modifier
+                .fillMaxWidth()
+//                .height(80.dp)
+        ) {
+            if (isSearchingNow) {
+                TextButton(onClick = { isSearchingNow = false }) {
+                    Icon(
+                        Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = ""
+                    )
+                }
+                SearchBar(
+                    onValueChange = { /* TODO */ },
+                    modifier = Modifier.weight(1f)
+                )
+            } else {
+                TextButton(
+                    onClick = { isSearchingNow = true },
+                ) {
+                    Icon(
+                        Icons.Default.Search,
+                        contentDescription = stringResource(R.string.search_icon_description)
+                    )
+                }
+            }
+            TextButton(
+                onClick = { /*TODO*/ },
+            ) {
+                Icon(
+                    painterResource(id = R.drawable.filter_icon),
+                    contentDescription = stringResource(R.string.filter_icon_description)
+                )
+            }
+            TextButton(
+                onClick = { /*TODO*/ },
+            ) {
+                Icon(
+                    Icons.Default.MoreVert,
+                    contentDescription = stringResource(R.string.more_options_icon_description)
+                )
+            }
         }
     }
 }
@@ -38,7 +99,7 @@ fun PokemonSearchBar(modifier: Modifier = Modifier) {
 @Composable
 fun PokemonColumnList(modifier: Modifier = Modifier) {
     LazyColumn(
-        modifier = modifier
+        modifier = modifier.fillMaxWidth()
     ) {
         item {
             val types = PokemonType.entries
--- app/src/main/java/com/barril/pokedexapp/ui/SearchBar.kt
+++ app/src/main/java/com/barril/pokedexapp/ui/SearchBar.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.barril.pokedexapp.R
 
@@ -28,7 +27,7 @@ fun SearchBar(
         },
         colors = TextFieldDefaults.colors(),
         placeholder = {
-            Text(stringResource(R.string.search))
+            Text(stringResource(R.string.search_icon_description))
         },
         modifier = modifier
             .fillMaxWidth()
