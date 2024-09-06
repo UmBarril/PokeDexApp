d8c207a11c8d1ec0191981d5b858582d25e45927
joao.barros <joao.barros@phoebustecnologia.com.br>
2024-09-06 14:45:32 -0300

    corrigindo posicionamento e tamanho dos itens em PokemonCard

--- app/src/main/java/com/barril/pokedexapp/ui/PokemonCard.kt
+++ app/src/main/java/com/barril/pokedexapp/ui/PokemonCard.kt
@@ -1,12 +1,18 @@
 package com.barril.pokedexapp.ui
 
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Star
@@ -15,81 +21,109 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.FilterQuality
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import com.barril.pokedexapp.PokemonType
 import com.barril.pokedexapp.R
+import com.bumptech.glide.integration.compose.GlideImage
 import java.util.EnumSet
 
 @Composable
 fun PokemonCard(pokemonName: String, pokemonType: EnumSet<PokemonType>, modifier: Modifier = Modifier) {
-    Card(
-        shape = MaterialTheme.shapes.medium,
-        modifier = modifier
-            .padding(10.dp, 4.dp)
-            .fillMaxWidth(),
+    var isExpanded by remember { mutableStateOf(true) }
+//    val normalIconSize = 24.dp
+//    val iconSize = animateDpAsState(targetValue = if (isExpanded) normalIconSize * 4 else 24.dp)
+//    val iconOffset = animateDpAsState(targetValue = if (isExpanded) -16.dp else 0.dp)
+
+    val paddingModifier = Modifier.padding(10.dp, 4.dp)
+    Box(
+        Modifier.padding(0.dp, 8.dp, 0.dp, 0.dp)
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically
+        val imageBitmap = ImageBitmap.imageResource(R.drawable.balbasaur)
+        val imageWidth = with(LocalDensity.current) {
+            imageBitmap.width.toDp()
+        }
+        Image(
+            bitmap = imageBitmap,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            filterQuality = FilterQuality.None,
+            modifier = paddingModifier
+                .offset(x = 28.dp, y = 4.dp)
+                .graphicsLayer { scaleX = 5f; scaleY = 5f }
+                .zIndex(2f)
+        )
+        Card(
+            shape = MaterialTheme.shapes.large,
+            modifier = paddingModifier.fillMaxWidth(),
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.balbasaur), // placeholder
-                contentDescription = null,
-//                contentScale = ContentScale.Crop,
-                modifier = modifier
-                    .size(150.dp)
-//                    .clip()
-            ) // aqui vai ter a imagem do pokemon
-            Column {
-                Text(
-                    text = pokemonName,
-                    color = Color.Black,
-                    fontFamily = FontFamily(
-                        Font(
-                            R.font.roboto_bold
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Spacer(Modifier.padding(imageWidth + 40.dp, 50.dp, 0.dp, 0.dp))
+                Column(
+                    modifier = Modifier.padding(4.dp)
+                ) {
+                    Text(
+                        text = pokemonName,
+                        color = Color.Black,
+                        fontFamily = FontFamily(
+                            Font(
+                                R.font.roboto_bold
+                            )
                         )
                     )
-                )
-                when {
-                    pokemonType.contains(PokemonType.FIRE) -> {
-                        Icon(Icons.Default.Clear, "")
-                    }
-                    pokemonType.contains(PokemonType.WATER) -> {
-                        Icon(Icons.Default.Clear, "")
-                    }
-                    pokemonType.contains(PokemonType.GRASS) -> {
-                        PokemonTypeIcons.GrassTypeIcon()
-                    }
-                    pokemonType.contains(PokemonType.ELECTRIC) -> {
-                        Icon(Icons.Default.Clear, "")
+                    for(type in pokemonType) {
+                        PokemonTypeIcons.fromPokemonType(type, 12.sp)
                     }
                 }
+
+                Spacer(Modifier.weight(1f))
+
+                // TODO: colocar propriedades de acessibilidade
+                Box(
+                    Modifier.background(Color.Green)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Star,
+                        contentDescription = stringResource(R.string.favorites_button_description),
+                        tint = Color.Yellow,
+                        modifier = Modifier
+                            .padding(20.dp, 0.dp)
+                            .clickable { },
+                    )
+                }
             }
-            // TODO: colocar propriedades de acessibilidade
-            Icon(
-                imageVector = Icons.Default.Star,
-                contentDescription = "Favoritos",
-                tint = Color.Yellow,
-                modifier = modifier
-                    .padding(10.dp, 0.dp)
-                    .clickable {  },
-            )
         }
-
     }
-
 }
 
 class RectangleShape : Shape {
--- app/src/main/java/com/barril/pokedexapp/ui/PokemonTypeIcon.kt
+++ app/src/main/java/com/barril/pokedexapp/ui/PokemonTypeIcon.kt
@@ -15,27 +15,37 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.barril.pokedexapp.PokemonType
 
 @Composable
 fun PokemonTypeIcon(
     typeName: String,
     backgroundColor: Color,
+    fontSize: TextUnit = 5.sp,
     modifier: Modifier = Modifier
 ) {
-    val borderSize = 1.5.dp
+//    val borderSize = 1.5.dp
+    val borderSize = with(LocalDensity.current) {
+        fontSize.toDp() / 4
+    }
+
+//  outlineWidth = 3f,
+    val outlineWidth = with(LocalDensity.current) {
+        fontSize.toPx() / 6
+    }
 
     // TODO: refatorar essa parte como: MultiOutlinedCard
     // primeira borda
     OutlinedCard(
         shape = MaterialTheme.shapes.medium,
-        border = BorderStroke(borderSize / 4, Color.Black),
-        colors = CardDefaults.cardColors(
-            containerColor = backgroundColor,
-        ),
+        border = BorderStroke(borderSize / 3, Color.Black),
         modifier = modifier
     ) {
         // segunda borda
@@ -54,11 +64,11 @@ fun PokemonTypeIcon(
                     text = typeName,
                     color = Color.White,
                     outlineColor = Color.Black,
-                    outlineWidth = 3f,
-                    outlineMiter = 3f,
+                    outlineWidth = outlineWidth,
+                    outlineMiter = 20f,
                     fontWeight = FontWeight.W800,
                     modifier = modifier.padding(4.dp),
-                    fontSize = 5.sp
+                    fontSize = fontSize
                 )
             }
         }
@@ -66,172 +76,217 @@ fun PokemonTypeIcon(
 }
 
 object PokemonTypeIcons {
+
     @Composable
-    fun GrassTypeIcon() {
+    fun GrassTypeIcon(fontSize: TextUnit) {
         PokemonTypeIcon(
             typeName = "GRASS",
+            fontSize = fontSize,
             backgroundColor = Color(0xFF7ED956)
         )
     }
 
     @Composable
-    fun FireTypeIcon() {
+    fun FireTypeIcon(fontSize: TextUnit) {
         PokemonTypeIcon(
             typeName = "FIRE",
+            fontSize = fontSize,
             backgroundColor = Color(0xFFF08030)
         )
     }
 
     @Composable
-    fun WaterTypeIcon() {
+    fun WaterTypeIcon(fontSize: TextUnit) {
         PokemonTypeIcon(
             typeName = "WATER",
+            fontSize = fontSize,
             backgroundColor = Color(0xFF6890F0)
         )
     }
 
     @Composable
-    fun ElectricTypeIcon() {
+    fun ElectricTypeIcon(fontSize: TextUnit) {
         PokemonTypeIcon(
             typeName = "ELECTRIC",
+            fontSize = fontSize,
             backgroundColor = Color(0xFFF8D030)
         )
     }
 
     @Composable
-    fun IceTypeIcon() {
+    fun IceTypeIcon(fontSize: TextUnit) {
         PokemonTypeIcon(
             typeName = "ICE",
+            fontSize = fontSize,
             backgroundColor = Color(0xFF98D8D8)
         )
     }
 
     @Composable
-    fun FightingTypeIcon() {
+    fun FightingTypeIcon(fontSize: TextUnit) {
         PokemonTypeIcon(
             typeName = "FIGHTING",
+            fontSize = fontSize,
             backgroundColor = Color(0xFFC03028)
         )
     }
 
     @Composable
-    fun PoisonTypeIcon() {
+    fun PoisonTypeIcon(fontSize: TextUnit) {
         PokemonTypeIcon(
             typeName = "POISON",
+            fontSize = fontSize,
             backgroundColor = Color(0xFFA040A0)
         )
     }
 
     @Composable
-    fun GroundTypeIcon() {
+    fun GroundTypeIcon(fontSize: TextUnit) {
         PokemonTypeIcon(
             typeName = "GROUND",
+            fontSize = fontSize,
             backgroundColor = Color(0xFFE0C068)
         )
     }
 
     @Composable
-    fun FlyingTypeIcon() {
+    fun FlyingTypeIcon(fontSize: TextUnit) {
         PokemonTypeIcon(
             typeName = "FLYING",
+            fontSize = fontSize,
             backgroundColor = Color(0xFFA890F0)
         )
     }
 
     @Composable
-    fun PsychicTypeIcon() {
+    fun PsychicTypeIcon(fontSize: TextUnit) {
         PokemonTypeIcon(
             typeName = "PSYCHIC",
+            fontSize = fontSize,
             backgroundColor = Color(0xFFF85888)
         )
     }
 
     @Composable
-    fun BugTypeIcon() {
+    fun BugTypeIcon(fontSize: TextUnit) {
         PokemonTypeIcon(
             typeName = "BUG",
+            fontSize = fontSize,
             backgroundColor = Color(0xFFA8B820)
         )
     }
 
     @Composable
-    fun RockTypeIcon() {
+    fun RockTypeIcon(fontSize: TextUnit) {
         PokemonTypeIcon(
             typeName = "ROCK",
+            fontSize = fontSize,
             backgroundColor = Color(0xFFB8A038)
         )
     }
 
     @Composable
-    fun GhostTypeIcon() {
+    fun GhostTypeIcon(fontSize: TextUnit) {
         PokemonTypeIcon(
             typeName = "GHOST",
+            fontSize = fontSize,
             backgroundColor = Color(0xFF705898)
         )
     }
 
     @Composable
-    fun DragonTypeIcon() {
+    fun DragonTypeIcon(fontSize: TextUnit) {
         PokemonTypeIcon(
             typeName = "DRAGON",
+            fontSize = fontSize,
             backgroundColor = Color(0xFF7038F8)
         )
     }
 
     @Composable
-    fun DarkTypeIcon() {
+    fun DarkTypeIcon(fontSize: TextUnit) {
         PokemonTypeIcon(
             typeName = "DARK",
+            fontSize = fontSize,
             backgroundColor = Color(0xFF705848)
         )
     }
 
     @Composable
-    fun SteelTypeIcon() {
+    fun SteelTypeIcon(fontSize: TextUnit) {
         PokemonTypeIcon(
             typeName = "STEEL",
+            fontSize = fontSize,
             backgroundColor = Color (0xFFB8B8D0)
         )
     }
 
     @Composable
-    fun FairyTypeIcon() {
+    fun FairyTypeIcon(fontSize: TextUnit) {
         PokemonTypeIcon(
            typeName = "FAIRY",
+           fontSize = fontSize,
            backgroundColor = Color (0xFFF0B6BC)
         )
     }
 
     @Composable
-    fun NormalTypeIcon() {
+    fun NormalTypeIcon(fontSize: TextUnit) {
         PokemonTypeIcon(
             typeName = "NORMAL",
+            fontSize = fontSize,
             backgroundColor = Color(0xFFA8A878)
         )
+    }
+
+    @Composable
+    fun fromPokemonType(type: PokemonType, fontSize: TextUnit) {
+        when(type) {
+            PokemonType.GRASS -> GrassTypeIcon(fontSize)
+            PokemonType.FIRE -> FireTypeIcon(fontSize)
+            PokemonType.WATER -> WaterTypeIcon(fontSize)
+            PokemonType.ELECTRIC -> ElectricTypeIcon(fontSize)
+            PokemonType.ICE -> IceTypeIcon(fontSize)
+            PokemonType.FIGHTING -> FightingTypeIcon(fontSize)
+            PokemonType.POISON -> PoisonTypeIcon(fontSize)
+            PokemonType.GROUND -> GroundTypeIcon(fontSize)
+            PokemonType.FLYING -> FlyingTypeIcon(fontSize)
+            PokemonType.PSYCHIC -> PsychicTypeIcon(fontSize)
+            PokemonType.BUG -> BugTypeIcon(fontSize)
+            PokemonType.ROCK -> RockTypeIcon(fontSize)
+            PokemonType.GHOST -> GhostTypeIcon(fontSize)
+            PokemonType.DRAGON -> DragonTypeIcon(fontSize)
+            PokemonType.DARK -> DarkTypeIcon(fontSize)
+            PokemonType.STEEL -> SteelTypeIcon(fontSize)
+            PokemonType.FAIRY -> FairyTypeIcon(fontSize)
+            PokemonType.NORMAL -> NormalTypeIcon(fontSize)
+            PokemonType.FIRE -> FireTypeIcon(fontSize)
+        }
     }
 }
 
 @Preview
 @Composable
 fun TypesPreview() {
+    val fontSize = 10.sp
     Column {
-        PokemonTypeIcons.GrassTypeIcon()
-        PokemonTypeIcons.FireTypeIcon()
-        PokemonTypeIcons.WaterTypeIcon()
-        PokemonTypeIcons.ElectricTypeIcon()
-        PokemonTypeIcons.IceTypeIcon()
-        PokemonTypeIcons.FightingTypeIcon()
-        PokemonTypeIcons.PoisonTypeIcon()
-        PokemonTypeIcons.GroundTypeIcon()
-        PokemonTypeIcons.FlyingTypeIcon()
-        PokemonTypeIcons.PsychicTypeIcon()
-        PokemonTypeIcons.BugTypeIcon()
-        PokemonTypeIcons.RockTypeIcon()
-        PokemonTypeIcons.GhostTypeIcon()
-        PokemonTypeIcons.DragonTypeIcon()
-        PokemonTypeIcons.DarkTypeIcon()
-        PokemonTypeIcons.SteelTypeIcon()
-        PokemonTypeIcons.FairyTypeIcon()
-        PokemonTypeIcons.NormalTypeIcon()
+        PokemonTypeIcons.GrassTypeIcon(fontSize)
+        PokemonTypeIcons.FireTypeIcon(fontSize)
+        PokemonTypeIcons.WaterTypeIcon(fontSize)
+        PokemonTypeIcons.ElectricTypeIcon(fontSize)
+        PokemonTypeIcons.IceTypeIcon(fontSize)
+        PokemonTypeIcons.FightingTypeIcon(fontSize)
+        PokemonTypeIcons.PoisonTypeIcon(fontSize)
+        PokemonTypeIcons.GroundTypeIcon(fontSize)
+        PokemonTypeIcons.FlyingTypeIcon(fontSize)
+        PokemonTypeIcons.PsychicTypeIcon(fontSize)
+        PokemonTypeIcons.BugTypeIcon(fontSize)
+        PokemonTypeIcons.RockTypeIcon(fontSize)
+        PokemonTypeIcons.GhostTypeIcon(fontSize)
+        PokemonTypeIcons.DragonTypeIcon(fontSize)
+        PokemonTypeIcons.DarkTypeIcon(fontSize)
+        PokemonTypeIcons.SteelTypeIcon(fontSize)
+        PokemonTypeIcons.FairyTypeIcon(fontSize)
+        PokemonTypeIcons.NormalTypeIcon(fontSize)
     }
 }
