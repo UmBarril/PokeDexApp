ca188b3cf839e38966b32744de246c227487c549
joao.barros <joao.barros@phoebustecnologia.com.br>
2024-09-13 16:48:55 -0300

    comentando implementação incompleta da interface da database para evitar erros de compilação

--- app/src/main/java/com/barril/pokedexapp/data/db/PokemonDatabase.kt
+++ app/src/main/java/com/barril/pokedexapp/data/db/PokemonDatabase.kt
@@ -3,7 +3,7 @@ package com.barril.pokedexapp.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [PokemonEntity::class], version = 1)
-abstract class PokemonDatabase : RoomDatabase() {
-    abstract fun pokemonDbDao(): PokemonDbDao
-}
+//@Database(entities = [PokemonEntity::class], version = 1)
+//abstract class PokemonDatabase : RoomDatabase() {
+//    abstract fun pokemonDbDao(): PokemonDbDao
+//}
--- app/src/main/java/com/barril/pokedexapp/data/db/PokemonDbDao.kt
+++ app/src/main/java/com/barril/pokedexapp/data/db/PokemonDbDao.kt
@@ -2,10 +2,10 @@ package com.barril.pokedexapp.data.db
 
 import androidx.room.Dao
 
-@Dao
-interface PokemonDbDao {
-//    @Query
-//    fun
-
-    fun insertPokemon()
-}
+//@Dao
+//interface PokemonDbDao {
+////    @Query
+////    fun
+//
+//    fun insertPokemon()
+//}
