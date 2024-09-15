b25d06108cb70016e2c6d056ddde3aa207f21cb6
joao.barros <joao.barros@phoebustecnologia.com.br>
2024-09-15 16:19:50 -0300

    adicionando mapper de PokemonEntity para Pokemon

--- app/src/main/java/com/barril/pokedexapp/data/Mappers.kt
+++ app/src/main/java/com/barril/pokedexapp/data/Mappers.kt
@@ -16,61 +16,6 @@ import com.barril.pokedexapp.domain.PokemonStat
 import com.barril.pokedexapp.domain.PokemonType
 import java.util.EnumSet
 
-fun PokemonDto.toPokemon(): Pokemon {
-    return Pokemon(
-        id = this.id,
-        name = this.name,
-        height = this.height,
-        order = this.order,
-        weight = this.weight,
-        isFavorite = false,
-        abilities = this.abilities.map {
-            PokemonAbility(
-                it.ability.name,
-                it.is_hidden,
-                it.ability.url,
-                it.slot
-            )
-        },
-        heldItems = this.held_items.map {
-            NamedResource(
-                name= it.item.name,
-                url = it.item.url
-            )
-        },
-        moves = this.moves.map {
-            NamedResource(
-                name = it.move.name,
-                url = it.move.url
-            )
-        },
-        sprites = this.sprites.let {
-            PokemonSpritesResource(
-                backDefaultUrl = it.back_default,
-                backFemaleUrl = it.back_female,
-                backShinyUrl = it.back_shiny,
-                backShinyFemaleUrl = it.back_shiny_female,
-                frontDefaultUrl = it.front_default,
-                frontFemaleUrl = it.front_female,
-                frontShinyUrl = it.front_shiny,
-                frontShinyFemaleUrl = it.front_shiny_female
-            )
-        },
-        cries = this.cries.let { PokemonCriesResource(it.latest, it.legacy) },
-        stats = this.stats.map {
-            PokemonStat(
-                name = it.stat.name,
-                baseStat = it.base_stat,
-                effort = it.effort,
-                url = it.stat.url
-            )
-        },
-        types = this.types.mapTo(EnumSet.noneOf(PokemonType::class.java)) {
-            PokemonType.fromString(it.type.name)
-        }
-    )
-}
-
 fun PokemonDto.toPokemonEntity(): PokemonEntity {
     return PokemonEntity(
         id = this.id,
@@ -127,6 +72,66 @@ fun PokemonDto.toPokemonEntity(): PokemonEntity {
         },
         types = this.types.map {
             it.type.name
+        }
+    )
+}
+
+fun PokemonEntity.toPokemon(): Pokemon {
+    return Pokemon (
+        id = this.id,
+        name = this.name,
+        height = this.height,
+        order = this.order,
+        weight = this.weight,
+        isFavorite = false,
+        abilities = this.abilities.map {
+            PokemonAbility (
+                name = it.name,
+                isHidden = it.isHidden,
+                url = it.url,
+                slot = it.slot
+            )
+        },
+        heldItems = this.heldItems.map {
+            NamedResource(
+                name = it.name,
+                url = it.url
+            )
+        },
+        moves = this.moves.map {
+            NamedResource(
+                name = it.name,
+                url = it.url
+            )
+        },
+        sprites = this.sprites.let {
+            PokemonSpritesResource(
+                backDefaultUrl = it.backDefaultUrl,
+                backFemaleUrl = it.backFemaleUrl,
+                backShinyUrl = it.backShinyUrl,
+                backShinyFemaleUrl = it.backShinyFemaleUrl,
+                frontDefaultUrl = it.frontDefaultUrl,
+                frontFemaleUrl = it.frontFemaleUrl,
+                frontShinyUrl = it.frontShinyUrl,
+                frontShinyFemaleUrl = it.frontFemaleUrl
+            )
+        },
+        cries = this.cries.let {
+            PokemonCriesResource(
+                latestUrl = it.latestUrl,
+                legacyUrl = it.legacyUrl
+            )
+        },
+        stats = this.stats.map {
+            PokemonStat(
+                name = it.name,
+                baseStat = it.baseStat,
+                effort = it.effort,
+                url = it.url
+            )
+        },
+        types = this.types.mapTo(EnumSet.noneOf(PokemonType::class.java)) {
+            PokemonType.fromString(it)
         }
     )
 }
