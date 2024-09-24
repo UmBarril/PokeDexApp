29ed2a0d71afe6e8e6ad703a0db25386f98a8a59
joao.barros <joao.barros@phoebustecnologia.com.br>
2024-09-24 06:30:26 -0300

    espaçamento

--- app/src/main/java/com/barril/pokedexapp/ui/screens/FavoritesScreen.kt
+++ app/src/main/java/com/barril/pokedexapp/ui/screens/FavoritesScreen.kt
@@ -2,6 +2,8 @@ package com.barril.pokedexapp.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
@@ -13,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.barril.pokedexapp.R
 import com.barril.pokedexapp.domain.Pokemon
@@ -33,6 +36,7 @@ fun FavoritesScreen(
             title = {
                 Row {
                     Text(stringResource(R.string.favorites_title))
+                    Spacer(Modifier.padding(10.dp))
                     Icon(
                         Icons.Default.Star,
                         contentDescription = ""
